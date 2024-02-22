9a26c4584882e4b4a442a1ff6f14a1adb7daa330
Rakesh_mishra <119526352+rakesh-mishra5719@users.noreply.github.com>
2024-02-22 10:09:49 +0530

    Add files via upload

---
+++ JumpGame.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        int reach = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if(i>reach) return false;
+            reach = Math.max(reach, i + nums[i]);
+        }
+        return true;
+    }
+}
+
+public class JumpGame {
+    public static void main(String[] args) {
+        Solution obj  = new Solution();
+        int nums[] = {2,3,1,1,4};
+        
+        System.out.println(obj.canJump(nums));
+    }
+}
